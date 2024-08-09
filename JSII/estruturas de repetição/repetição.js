036759f80c45afe0af4c19f32d3aae105ce0fefb
jfandre00 <jfandre@gmail.com>
2024-08-08 22:07:52 -0300

    add exercício de repetição

---
+++ JSII/estruturas de repetição/repetição.js
@@ -0,0 +1,18 @@
+// for
+for (let i=1; i < 6; i++) {
+    console.log(i)
+}
+
+// while - checa a validação antes de executar o primeiro loop
+let i = 0;
+while (i < 5) {
+    console.log(i+1);
+    i++;
+}
+
+// dowhile - faz o primeiro loop depois checa a validação
+i = 0;
+do {
+    console.log(i+1);
+    i++;
+} while (i < 5)
